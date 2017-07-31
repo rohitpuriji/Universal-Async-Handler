0f8d46c90dd7fc2e63d11c2ecb954613f51d1180
Rohit <rohit.puri@evontech.com>
2017-07-31 12:49:58 +0530

    changes in build gradle

--- app/src/main/java/com/tech/myapplication/MainActivity.java
+++ app/src/main/java/com/tech/myapplication/MainActivity.java
@@ -3,10 +3,10 @@ package com.tech.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.tech.universalasynchandler.AppLogs;
+/*import com.tech.universalasynchandler.AppLogs;
 import com.tech.universalasynchandler.HttpPostCaller;
 
-import org.json.JSONObject;
+import org.json.JSONObject;*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HttpPostCaller httpPostCaller = new HttpPostCaller();
+ /*       HttpPostCaller httpPostCaller = new HttpPostCaller();
         Object responseObject = httpPostCaller.getResponse(MainActivity.this,"https://api.androidhive.info/contacts/",new JSONObject(),"Please wait....");
         if (responseObject instanceof  String){
             AppLogs.printLogs("response :",responseObject.toString());
-        }
+        }*/
     }
 }
